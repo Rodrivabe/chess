84911b2d909c48865405d81415ddeee98765bdaa
Rodrivabe <150090269+Rodrivabe@users.noreply.github.com>
2025-04-07 17:06:24 -0600

    Added Message and Command base classes

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,18 @@
 package server.websocket;
 
+import java.util.concurrent.ConcurrentHashMap;
+import org.eclipse.jetty.websocket.api.Session;
+
+
 public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, Connection> connections = new ConcurrentHashMap<>();
+
+    public void saveSession(int gameID, Session session) {
+        var connection = new Connection(gameID, session);
+        connections.put(gameID, connection);
+    }
+
+    public void remove(int gameID) {
+        connections.remove(gameID);
+    }
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,7 +1,11 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+
 
 import java.io.IOException;
 
@@ -12,9 +16,25 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
-        Action action = new Gson().fromJson(message, Action.class);
-        switch (action.type()) {
-            case ENTER -> enter(action.visitorName(), session);
-            case EXIT -> exit(action.visitorName());
+        try{
+            UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+            String username = getUsername(command.getAuthToken());
+
+        }
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        UserGameCommand.CommandType type = command.getCommandType();
+        switch (type) {
+            case CONNECT -> connect(session, username, (ConnectCommand) command);
+            case MAKE_MOVE -> exit(command.visitorName());
+            case LEAVE -> ;
+            case RESIGN ->;
         }
     }
+
+    private void connect(String visitorName, Session session) throws IOException {
+        connections.add(visitorName, session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new Notification(Notification.Type.ARRIVAL, message);
+        connections.broadcast(visitorName, notification);
+    }
+}
